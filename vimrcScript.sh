e329ca017ce6510c67cd0285c900f9d8e0f4a55a
jlekas <james.lekas@threatstack.com>
2016-08-08 19:51:54 -0400

    starwars bithces

--- vimrcScript.sh
+++ vimrcScript.sh
@@ -2,3 +2,10 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
 vim +PluginInstall +qall
+
+//for ubuntu
+sudo apt-get install cowsay
+cowsay -f dragon-and-cow Roar "I'm a dragon"
+cowthink "Hi"
+telnet towel.blinkenlights.nl
+
